8535c3bfd0274650b6e130423bafce2b443afb1f
Vladislav Kovalchitskii <progerkovalenko@gmail.com>
2026-01-18 20:05:45 +0700

    feat: add core request logic, weather data visualization

--- WeatherAPIMain.py
+++ WeatherAPIMain.py
@@ -1,1 +1,38 @@
+import os
 
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("API_KEY")
+
+
+def get_weather(city:str):
+
+    base_url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
+
+    params = {
+        'unitGroup': 'metric',
+        'key': api_key,
+        'contentType': 'json'
+    }
+
+    try:
+        response = requests.get(f'{base_url}{city}', params=params)
+        response.raise_for_status()
+        return response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Connection error: {e}")
+        return None
+
+if __name__ == "__main__":
+    city_name = input("Enter city name: ")
+    weather_data = get_weather(city_name)
+
+    if weather_data:
+        currentWeather = weather_data["currentConditions"]
+        print(f"\nWeather in {weather_data['address']}:")
+        print(f"Temperature: {currentWeather['temp']}C")
+        print(f"Humidity: {currentWeather['humidity']}%")
+        print(f"Condition: {currentWeather['conditions']}")
